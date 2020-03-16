414f0eedc0c18ff0b72a4064bc360c26994a833c
yongdol503 <32837560+yongdol503@users.noreply.github.com>
2020-03-16 23:35:41 +0900

    add

---
+++ YONG_YG/2167.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int dp[300][300];
+int sum, n, m, k, a, b, x, y;
+
+int main()
+{
+    cin>>n>>m;
+    for(int i=0; i<n; i++)
+        for(int j=0; j<m; j++)
+            cin>>dp[i][j];
+    cin>>k;
+    for(int i=0; i<k; i++)
+    {
+        cin>>a>>b>>x>>y;
+        sum=0;
+        
+    }
+}
---
+++ YONG_YG/2908.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char a[4], b[4];
+    int an=0, bn=0;
+    cin>>a>>b;
+    for(int i=0; i<3; i++)
+    {
+        
+    }
+}
